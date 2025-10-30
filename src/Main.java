f15b8e9e5b3a4080be046d82ccd8e11cceadeb94
abhaysinghal126 <abhaysinghal126@gmail.com>
2025-10-30 22:17:22 +0200

    feat: add functional Main.java demonstrating static print method with multiple value types

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
+import java.util.*;
 
-public class Main{
-    public static void main(final String[] args){
-        //Code goes here
+/**
+ * This is a simplified Java fundamentals demo.
+ * It demonstrates classes, objects, and a static method.
+ * The static method 'print' can print different types of values: strings, numbers, booleans, etc.
+ *
+ * @author Abhay Singhal
+ */
+
+public class Main {
+
+    /**
+     * Prints any object using a static method.
+     * This method is flexible and can print values of different types:
+     * - Strings
+     * - Integers
+     * - Doubles
+     * - Booleans
+     * - Any other Object
+     *
+     * @param obj The object to print
+     */
+    static void print(Object obj) {
+        System.out.println(obj);
+    }
+
+    /**
+     * Entry point of the program.
+     * Demonstrates using the static 'print' method with different types of values.
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Using the static method to print different types of values
+        print("Hello, World!");  // printing a string
+        print(42);               // printing an integer
+        print(3.14);             // printing a double
+        print(true);             // printing a boolean
+
+        // Demonstrates that you can also print objects
+        Date now = new Date();
+        print(now);              // printing an object
     }
 }
